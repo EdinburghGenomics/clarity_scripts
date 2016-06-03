31e58dc3e5dd20060a7694280d9cb212b1b1e975
tcezard <timothee.cezard@ed.ac.uk>
2016-06-03 16:57:47 +0100

    Fix couple of bugs add debug msgs

--- EPPs/prepare_discard_plate.py
+++ EPPs/prepare_discard_plate.py
@@ -35,17 +35,23 @@ discard_wf_name="Discard Plates EG 1.0"
 sample_discard_wf_stage_name = "Find Derived Sample Plates EG 1.0"
 plate_discard_wf_stage_name="Discard Plates EG 1.0"
 
+def batch_limit(lims, list_instance, max_query=100):
+    for start in range(0,len(list_instance), max_query):
+        print("retrieve %s"%len(list_instance[start:start+max_query]))
+        lims.get_batch(list_instance[start:start+max_query])
+
 def fetch_all_artifacts_for_samples(lims, samples):
     """
     search for all the artifact associated with a list of samples.
-    To avoid too long a query we limit to 100 samples per query
+    To avoid too long a query we limit to 50 samples per query
     """
     lims_ids = [s.id for s in samples]
-    max_query = 100
+    max_query = 50
     artifacts = []
     for start in range(0,len(lims_ids), max_query):
-        artifacts.extend(lims.get_artifacts(samplelimsid=lims_ids[start:start+max_query]))
-    lims.get_batch(artifacts)
+        artifacts.extend(lims.get_artifacts(samplelimsid=lims_ids[start:start+max_query], type="Analyte"))
+    print("Found %s artifacts. Now need to batch retrieve them"%len(artifacts))
+    batch_limit(lims, artifacts)
     return artifacts
 
 def is_valid_container(container):
@@ -79,6 +85,8 @@ def get_workflow_stage(lims, workflow_name, stage_name=None):
         return
     return stages[0]
 
+
+
 def main():
     args = _parse_args()
     r1 = urlparse.urlsplit(args.step_uri)
@@ -89,8 +97,8 @@ def main():
     lims = Lims(server_http, args.username, args.password)
 
     #Find the Discard plate workflow uri
-    discard_plate_stage_uri = get_workflow_stage(lims, workflow_name=discard_wf_name, stage_name=plate_discard_wf_stage_name)
-
+    discard_plate_stage = get_workflow_stage(lims, workflow_name=discard_wf_name, stage_name=plate_discard_wf_stage_name)
+    print("Found discard_plate_stage_uri "+discard_plate_stage.uri)
     #get the process currently running
     p = Process(lims, id=step_id)
 
@@ -101,23 +109,31 @@ def main():
     #fetch the sample associated with these artifacts
     samples = [a.samples[0] for a in step_artifacts]
     lims.get_batch(samples)
-
+    print("Found %d Samples"%len(samples))
     #fetch the all the artifacts associated with these samples
-    step_associated_artifacts = fetch_all_artifacts_for_samples(samples)
-
+    step_associated_artifacts = fetch_all_artifacts_for_samples(lims, samples)
+    print("Found %d step_associated_artifacts"%len(step_associated_artifacts))
     #list all the containers and retrive them
     containers = list(set([a.container for a in step_associated_artifacts]))
-    lims.get_batch(containers)
+    batch_limit(lims, containers)
+    print("Found %d containers"%len(containers))
 
     #filter the containers to keep the one that are valid
     valid_containers = [c for c in containers if is_valid_container(c)]
+    print("Found %d valid_containers"%len(valid_containers))
 
     #Get all the artifacts in the valid containers and retrieve the one that were not retrieved already
-    container_artifacts = set(sum([list(c.placements.values()) for c in valid_containers]))
+    container_artifacts = set()
+    for c in valid_containers:
+        container_artifacts.update( set(c.placements.values()) )
+
     non_step_atifacts = container_artifacts.difference(set(step_associated_artifacts))
-    lims.get_batch(non_step_atifacts)
+    print("Found %d non_step_atifacts"%len(non_step_atifacts))
+    batch_limit(lims, non_step_atifacts)
+    print("Found %d non_step_atifacts"%len(non_step_atifacts))
 
-
+    artifacts_to_route = []
+    container_to_route = []
     for container in valid_containers:
         route_allowed=True
         for artifact in list(container.placements.values()):
@@ -127,12 +143,12 @@ def main():
                 # This container will have to wait
                 route_allowed=False
         if route_allowed:
-            lims.route_artifacts(list(container.placements.values()), stage_uri=discard_plate_stage_uri)
+            artifacts_to_route.extend(list(container.placements.values()))
+            container_to_route.append(container)
+    print("Route %s containers with %s artifacts"%(len(container_to_route), len(artifacts_to_route)))
+    lims.route_artifacts(artifacts_to_route, stage_uri=discard_plate_stage.uri)
 
-
-
-
-
+    #TODO: clean up steps where the step_associated_artifacts are queued
 
 def _parse_args():
     p = argparse.ArgumentParser()
