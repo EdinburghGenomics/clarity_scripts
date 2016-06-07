80f02455b8a434330931f32cdada32ece12527c2
tcezard <timothee.cezard@ed.ac.uk>
2016-06-07 15:44:45 +0100

    Add logging to file

--- EPPs/prepare_discard_plate.py
+++ EPPs/prepare_discard_plate.py
@@ -1,10 +1,8 @@
 import argparse
 import logging
-import os
-import re
-from sys import version_info
+import sys
 
-if version_info.major == 2:
+if sys.version_info.major == 2:
     import urlparse
 else:
     from urllib import parse as urlparse
@@ -34,6 +32,10 @@ invalid_suffixes = [
 discard_wf_name="Discard Plates EG 1.0"
 sample_discard_wf_stage_name = "Find Derived Sample Plates EG 1.0"
 plate_discard_wf_stage_name="Discard Plates EG 1.0"
+
+
+logger = logging.getLogger(__name__)
+
 
 def batch_limit(lims, list_instance, max_query=100):
     for start in range(0,len(list_instance), max_query):
@@ -86,19 +88,11 @@ def get_workflow_stage(lims, workflow_name, stage_name=None):
     return stages[0]
 
 
-
-def main():
-    args = _parse_args()
-    r1 = urlparse.urlsplit(args.step_uri)
-    server_http = '%s://%s'%(r1.scheme, r1.netloc)
-
-    #Assume the step_uri contains the step id at the end
-    step_id = r1.path.split('/')[-1]
-    lims = Lims(server_http, args.username, args.password)
-
+def find_plate_to_route(lims, step_id):
     #Find the Discard plate workflow uri
-    discard_plate_stage = get_workflow_stage(lims, workflow_name=discard_wf_name, stage_name=plate_discard_wf_stage_name)
-    print("Found discard_plate_stage_uri "+discard_plate_stage.uri)
+    discard_plate_stage = get_workflow_stage(lims, workflow_name=discard_wf_name,
+                                             stage_name=plate_discard_wf_stage_name)
+    logger.info("Found discard_plate_stage_uri "+discard_plate_stage.uri)
     #get the process currently running
     p = Process(lims, id=step_id)
 
@@ -109,18 +103,18 @@ def main():
     #fetch the sample associated with these artifacts
     samples = [a.samples[0] for a in step_artifacts]
     lims.get_batch(samples)
-    print("Found %d Samples"%len(samples))
+    logger.info("Found %d Samples"%len(samples))
     #fetch the all the artifacts associated with these samples
     step_associated_artifacts = fetch_all_artifacts_for_samples(lims, samples)
-    print("Found %d step_associated_artifacts"%len(step_associated_artifacts))
+    logger.info("Found %d step_associated_artifacts"%len(step_associated_artifacts))
     #list all the containers and retrive them
     containers = list(set([a.container for a in step_associated_artifacts]))
     batch_limit(lims, containers)
-    print("Found %d containers"%len(containers))
+    logger.info("Found %d containers"%len(containers))
 
     #filter the containers to keep the one that are valid
     valid_containers = [c for c in containers if is_valid_container(c)]
-    print("Found %d valid_containers"%len(valid_containers))
+    logger.info("Found %d valid_containers"%len(valid_containers))
 
     #Get all the artifacts in the valid containers and retrieve the one that were not retrieved already
     container_artifacts = set()
@@ -128,9 +122,9 @@ def main():
         container_artifacts.update( set(c.placements.values()) )
 
     non_step_atifacts = container_artifacts.difference(set(step_associated_artifacts))
-    print("Found %d non_step_atifacts"%len(non_step_atifacts))
+    logger.info("Found %d non_step_atifacts"%len(non_step_atifacts))
     batch_limit(lims, non_step_atifacts)
-    print("Found %d non_step_atifacts"%len(non_step_atifacts))
+    logger.info("Found %d non_step_atifacts"%len(non_step_atifacts))
 
     artifacts_to_route = []
     container_to_route = []
@@ -145,18 +139,40 @@ def main():
         if route_allowed:
             artifacts_to_route.extend(list(container.placements.values()))
             container_to_route.append(container)
+    logger.info("Route %s containers with %s artifacts"%(len(container_to_route), len(artifacts_to_route)))
     print("Route %s containers with %s artifacts"%(len(container_to_route), len(artifacts_to_route)))
     lims.route_artifacts(artifacts_to_route, stage_uri=discard_plate_stage.uri)
 
     #TODO: clean up steps where the step_associated_artifacts are queued
 
+
+def main():
+    args = _parse_args()
+    r1 = urlparse.urlsplit(args.step_uri)
+    server_http = '%s://%s'%(r1.scheme, r1.netloc)
+
+    #Assume the step_uri contains the step id at the end
+    step_id = r1.path.split('/')[-1]
+    lims = Lims(server_http, args.username, args.password)
+
+    #setup logging
+    formatter = logging.Formatter(
+            fmt='[%(asctime)s] [%(levelname)s] %(message)s',
+            datefmt='%Y-%b-%d %H:%M:%S'
+        )
+    handler = logging.FileHandler(args.log_file)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return(find_plate_to_route(lims, step_id))
+
 def _parse_args():
     p = argparse.ArgumentParser()
-    p.add_argument('--username', dest="username", type=str, help='The username of the person logged in')
-    p.add_argument('--password', dest="password", type=str, help='The password used by the person logged in')
-    p.add_argument('--step_uri', dest='step_uri', type=str, help='The uri of the step this EPP is attached to')
+    p.add_argument('--username', dest="username", type=str, required=True, help='The username of the person logged in')
+    p.add_argument('--password', dest="password", type=str, required=True, help='The password used by the person logged in')
+    p.add_argument('--step_uri', dest='step_uri', type=str, required=True, help='The uri of the step this EPP is attached to')
+    p.add_argument('--log_file', dest='log_file', type=str, required=True, help='The log file containing statement about what was done')
     return p.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
