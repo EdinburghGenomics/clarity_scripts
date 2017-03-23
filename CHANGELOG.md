1b8cab4c57c02d26ea949b7366597d83aa61c7ca
tcezard <timothee.cezard@ed.ac.uk>
2017-03-23 16:52:30 +0000

    Use EGCG-core 0.6.9 to get asana 0.6.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,9 @@
-Changelog for EGCG-Core
-===========================
+Changelog for clarity_scripts
+=============================
 
 0.2 (unreleased)
-------------------
+----------------
+ - add accufill check in convert_and_dispatch_genotypes
  - Support for installing via setup.py
 
 0.1.1
--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 import glob
-import subprocess
 from setuptools import setup, find_packages
 from os.path import join, abspath, dirname
-import pip
-
-# Fetch version from git tags.
-#version = subprocess.Popen(["git", "describe", "--abbrev=0"],stdout=subprocess.PIPE, universal_newlines=True).communicate()[0].rstrip()
-#version = version.decode("utf-8")
 
 requirements_txt = join(abspath(dirname(__file__)), 'requirements.txt')
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 requirements = [l.strip() for l in open(requirements_txt) if l and not l.startswith('#')]
-
 
 version = '0.1.1'
 
