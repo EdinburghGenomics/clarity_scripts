67be67b18120aa8250012c25216f412464d2688f
tcezard <timothee.cezard@ed.ac.uk>
2017-04-24 15:46:18 +0100

    Add scripts from production that have been migrated to python3

---
+++ prodscripts/Autoplacement96.py
@@ -0,0 +1,87 @@
+__author__ = 'dcrawford'
+
+from optparse import OptionParser
+from xml.dom.minidom import parseString
+
+from EPPs import glsapiutil
+
+
+def autoPlace():
+    stepdetailsXML = api.GET(args.stepURI + "/details")
+    stepdetails = parseString(stepdetailsXML)
+
+    ## Create the input output map
+    iomap = {}
+    for io in stepdetails.getElementsByTagName("input-output-map"):
+
+        output = io.getElementsByTagName("output")[0]
+        if output.getAttribute("type") == "Analyte":
+            input = io.getElementsByTagName("input")[0]
+            iomap[output.getAttribute("uri")] = input.getAttribute("uri")
+
+    artifacts = parseString(api.getArtifacts(list(iomap.keys()) + list(iomap.values())))
+
+    ## Map the original locations of the artfacts
+    inputMap = {}
+    for art in artifacts.getElementsByTagName("art:artifact"):
+
+        artURI = art.getAttribute("uri").split("?state")[0]
+        if artURI in list(iomap.values()):
+            location = art.getElementsByTagName("container")[0].getAttribute("uri")
+            well = art.getElementsByTagName("value")[0].firstChild.data
+            inputMap[artURI] = [location, well]
+
+    inputContainers = list(set([c[0] for c in list(
+        inputMap.values())]))  # if the order of containers is going to be important, sort them here <-
+    stepplacementsXML = api.GET(args.stepURI + "/placements")
+    stepplacements = parseString(stepplacementsXML)
+    output384 = stepplacements.getElementsByTagName("container")[0].getAttribute("uri")
+
+    # writing the new placement XML from scratch
+    sendplacement = [
+        '<stp:placements xmlns:stp="http://genologics.com/ri/step" uri="' + args.stepURI + '/placements"><step uri="' + args.stepURI + '" rel="steps"/><output-placements>']
+    for outArt in stepplacements.getElementsByTagName("output-placement"):
+        outArtURI = outArt.getAttribute("uri")
+        inputLocation = inputMap[iomap[outArtURI]]
+        containerIndex = inputContainers.index(inputLocation[0])
+        sourceWell = inputLocation[1]
+
+        # if len( inputContainers ) == 4:
+        value = sourceWell
+        # else:
+        #   value = wheredoesitgo_1to3plates( containerIndex, sourceWell )
+
+        sendplacement.append(
+            '<output-placement uri="' + outArtURI + '"><location><container uri="' + output384 + '" limsid="' +
+            output384.split("/containers/")[1] + '"/><value>' + value + '</value></location></output-placement>')
+    sendplacement.append('</output-placements></stp:placements>')
+
+    r = api.POST(''.join(sendplacement), args.stepURI + "/placements")
+
+
+def setupArguments():
+    Parser = OptionParser()
+    Parser.add_option('-u', "--username", action='store', dest='username')
+    Parser.add_option('-p', "--password", action='store', dest='password')
+    Parser.add_option('-s', "--stepURI", action='store', dest='stepURI')
+
+    return Parser.parse_args()[0]
+
+
+api = None
+
+
+def main():
+    global args
+    args = setupArguments()
+
+    global api
+    api = glsapiutil.glsapiutil2()
+    api.setURI(args.stepURI)
+    api.setup(args.username, args.password)
+
+    autoPlace()
+
+
+if __name__ == "__main__":
+    main()
---
+++ prodscripts/QuantStudio/QuantStudio_hamilton.py
@@ -0,0 +1,94 @@
+__author__ = 'dcrawford'
+
+import glsapiutil
+from xml.dom.minidom import parseString
+from optparse import OptionParser
+
+api = None
+
+def limslogic():
+
+    def writecsv():
+
+        content = ['Input Plate, Input Well, Output Plate, Output Well']
+
+        for art in sortedArtifacts:
+
+            inputContainerName = ConName[ inputMap[ art ][0] ]
+            inputWell = inputMap[ art ][1]
+            outputArt = iomap[ art ]
+            outputContainerName = ConName[ inputMap[ outputArt ][0] ]
+            outputWell = inputMap[ outputArt ][1]
+
+            content.append( ','.join( [ inputContainerName,
+                                        inputWell,
+                                        outputContainerName,
+                                        outputWell ]) )
+
+        content = '\n'.join( content )
+        file = open( options.resultfile , "w" )
+        file.write( content )
+        file.close()
+
+    # Gather the required Data from LIMS
+    stepdetailsXML = api.GET( options.stepURI + "/details" )
+    stepdetails = parseString( stepdetailsXML )
+
+    ## Create the input output map
+    iomap = {}
+    for io in stepdetails.getElementsByTagName( "input-output-map" ):
+        output = io.getElementsByTagName( "output" )[0]
+        if output.getAttribute( "type" ) == "Analyte":
+            input = io.getElementsByTagName( "input" )[0]
+            iomap[ input.getAttribute( "uri" ) ] = output.getAttribute( "uri" )
+
+    artifacts = parseString( api.getArtifacts( list(iomap.keys()) + list(iomap.values())) )
+
+    ## Map the locations of the artfacts
+    inputMap = {}
+    for art in artifacts.getElementsByTagName( "art:artifact" ):
+        artURI = art.getAttribute( "uri" ).split("?state")[0]
+        location = art.getElementsByTagName( "container" )[0].getAttribute( "uri" )
+        well = art.getElementsByTagName( "value" )[0].firstChild.data
+        inputMap[ artURI ] = [ location, well ]
+
+    # Gather the names of the containers
+    ConName = {}
+    for c in parseString( api.getContainers( list( set([ c[0] for c in list(inputMap.values()) ])) )).getElementsByTagName("con:container"):
+        ConName[ c.getAttribute( "uri" ) ] = c.getElementsByTagName("name")[0].firstChild.data
+
+    ## sort the artifacts by the container first, then the well location
+    def con_well(x):
+        w = inputMap[x][1]
+        if len( inputMap[x][1] ) == 3:
+            #w = w[:2] + "0" + w[2:] ## row first
+            w = "0" + w[2:] + w[:2] ## column first
+        return inputMap[x][0] + w
+    sortedArtifacts = sorted( list(iomap.keys()), key=con_well)
+
+    writecsv()
+
+def setupArguments():
+
+    Parser = OptionParser()
+    Parser.add_option('-u', "--username", action='store', dest='username')
+    Parser.add_option('-p', "--password", action='store', dest='password')
+    Parser.add_option('-s', "--stepURI", action='store', dest='stepURI')
+    Parser.add_option('-r', "--resultfile", action='store', dest='resultfile')
+
+    return Parser.parse_args()[0]
+
+def main():
+
+    global options
+    options = setupArguments()
+
+    global api
+    api = glsapiutil.glsapiutil2()
+    api.setURI( options.stepURI )
+    api.setup( options.username, options.password )
+
+    limslogic()
+
+if __name__ == "__main__":
+    main()
