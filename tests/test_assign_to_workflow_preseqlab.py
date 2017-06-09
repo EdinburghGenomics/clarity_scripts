ffc2aa9969bc7ab82e01197d73aff2c64db75621
tcezard <timothee.cezard@ed.ac.uk>
2017-06-09 10:43:39 +0100

    Add extra test to check that FluidX samples are routed the right way

--- tests/test_assign_to_workflow_preseqlab.py
+++ tests/test_assign_to_workflow_preseqlab.py
@@ -1,7 +1,8 @@
 from EPPs.common import StepEPP
 from scripts.assign_workflow_preseqlab import AssignWorkflowPreSeqLab
 from tests.test_common import TestEPP, fake_artifact
-from unittest.mock import Mock, patch, PropertyMock
+from unittest.mock import Mock, patch, PropertyMock, MagicMock
+
 
 def fake_all_inputs(unique=False, resolve=False):
     '''Return a list of mocked artifacts which contain sample which contain artifact ... Simple!'''
@@ -16,8 +17,18 @@ def fake_all_inputs_fluidx(unique=False, resolve=False):
         Mock(samples=[Mock(artifact=fake_artifact(id='a1'), id='s1', udf={
             "Proceed To SeqLab": True,
             "2D Barcode": 'fluidX1'
-        })])
+        })]),
     )
+
+
+class MockedLims(MagicMock):
+    def get_artifacts(*args, **kwargs):
+        return [
+            Mock(id='fx1', parent_process=Mock(id='121')),
+            Mock(id='fx2', parent_process=Mock(id='123'))
+        ]
+
+
 
 class TestAssignWorkflowPreSeqLab(TestEPP):
     def setUp(self):
@@ -33,6 +44,11 @@ class TestAssignWorkflowPreSeqLab(TestEPP):
         )
 
         self.patched_lims = patch.object(AssignWorkflowPreSeqLab, 'lims', new_callable=PropertyMock)
+        self.patched_get_workflow_stage = patch(
+            'scripts.assign_workflow_preseqlab.get_workflow_stage',
+            return_value=Mock(uri='a_uri')
+        )
+
         self.epp = AssignWorkflowPreSeqLab(
             'http://server:8080/a_step_uri',
             'a_user',
@@ -41,11 +57,18 @@ class TestAssignWorkflowPreSeqLab(TestEPP):
         )
 
     def test_assign(self):
-        with patch('scripts.assign_workflow_preseqlab.get_workflow_stage', return_value=Mock(uri='a_uri')) as p, \
-             self.patched_lims, self.patched_process:
+        with self.patched_get_workflow_stage as pws, self.patched_lims, self.patched_process:
             self.epp._run()
-            p.assert_called_with(self.epp.lims, 'PreSeqLab EG 6.0', 'Sequencing Plate Preparation EG 2.0')
-            exp_artifacts = ['a1', 'a2']
 
-            assert sorted([a.id for a in self.epp.lims.route_artifacts.call_args[0][0]]) == exp_artifacts
+            pws.assert_called_with(self.epp.lims, 'PreSeqLab EG 6.0', 'Sequencing Plate Preparation EG 2.0')
+            assert sorted([a.id for a in self.epp.lims.route_artifacts.call_args[0][0]]) == ['a1', 'a2']
+            assert self.epp.lims.route_artifacts.call_args[1] == {'stage_uri': 'a_uri'}
+
+    def test_assign_fluidx(self):
+        with self.patched_get_workflow_stage as pws, self.patched_lims as pl, self.patched_process_fluidx:
+            pl.return_value = MockedLims()
+            self.epp._run()
+
+            pws.assert_called_with(self.epp.lims, 'PreSeqLab EG 6.0', 'Sequencing Plate Preparation EG 2.0')
+            assert sorted([a.id for a in self.epp.lims.route_artifacts.call_args[0][0]]) == ['fx2']
             assert self.epp.lims.route_artifacts.call_args[1] == {'stage_uri': 'a_uri'}
