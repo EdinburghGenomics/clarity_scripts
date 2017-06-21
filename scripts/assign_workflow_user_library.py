906e93d5b7a3c95bb3b42250c2721b1c8fde80a6
dforth <dan.forth@ed.ac.uk>
2017-06-21 16:32:17 +0100

    New script for assigning samples from step User Prepared Libraries Batch to TruSeq Nano or TruSeq PCR-Free

---
+++ scripts/assign_workflow_user_library.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from EPPs.common import StepEPP, step_argparser, get_workflow_stage, find_newest_artifact_originating_from
+
+
+def get_parent_process_id(art):
+    return art.parent_process.id
+
+
+class AssignWorkflowUserPreparedLibrary(StepEPP):
+    #Assign plate created in User Prepared Library to either Nano or PCR Free workflow
+
+    def _run(self):
+        artifact_to_route_pcr_free = set()
+        artifact_to_route_nano = set()
+        artifact_to_route_quant = set()
+
+        for art in self.output_artifacts:
+            sample = art.samples[0]
+            if sample.udf.get("Prep Workflow") == "TruSeq PCR-Free DNA Sample Prep":
+                artifact_to_route_pcr_free.add(art)
+            # assigns the normalised batch plate to the TruSeq PCR-Free workflow
+
+
+            elif sample.udf.get("Prep Workflow") == "TruSeq Nano DNA Sample Prep":
+                artifact_to_route_nano.add(art)
+                # assigns the normalised batch plate to the TruSeq Nano workflow
+
+        if artifact_to_route_pcr_free:
+            # Only route artifacts if there are any artifacts to go to PCR-Free
+            stage = get_workflow_stage(self.lims, "TruSeq PCR-Free DNA Sample Prep", "SEMI-AUTOMATED - Make and Read qPCR Quant")
+            self.lims.route_artifacts(list(artifact_to_route_pcr_free), stage_uri=stage.uri)
+
+        if artifact_to_route_nano:
+            # Only route artifacts if there are any artifacts to go to Nano
+            stage = get_workflow_stage(self.lims, "TruSeq Nano DNA Sample Prep", "SEMI-AUTOMATED - Make LQC & Caliper GX QC")
+            self.lims.route_artifacts(list(artifact_to_route_nano), stage_uri=stage.uri)
+
+
+
+def main():
+    p = step_argparser()
+    args = p.parse_args()
+    action = AssignWorkflowUserPreparedLibrary(args.step_uri, args.username, args.password, args.log_file)
+    action.run()
+
+
+if __name__ == "__main__":
+    main()
