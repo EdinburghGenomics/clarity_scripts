32d8e9ab176636463e3aa9745a33010e59c23f78
dforth <dan.forth@ed.ac.uk>
2017-06-23 13:26:43 +0100

    Artifacts to route corrected

--- scripts/assign_workflow_receive_sample.py
+++ scripts/assign_workflow_receive_sample.py
@@ -17,23 +17,23 @@ class AssignWorkflowReceiveSample(StepEPP):
         for art in self.artifacts:
             sample = art.samples[0]
             if sample.udf.get("User Prepared Library") == "Yes":
-                artifact_to_route_pcr_free.add(art)
+                artifact_to_route_userprepared.add(art)
             # assigns the received sample to User Prepared Library Batch if is a user prepared library
 
 
             elif not sample.udf.get("User Prepared Library"):
-                artifact_to_route_nano.add(art)
+                artifact_to_route_preseqlab.add(art)
                 # assigns the received sample to PreSeqLab if NOT an user prepared library
 
         if artifact_to_route_userprepared:
             # Only route artifacts if there are any artifacts to go to PCR-Free
             stage = get_workflow_stage(self.lims, "User Prepared Library Batch EG1.0 WF", "User Prepared Library Batch EG 1.0 ST")
-            self.lims.route_artifacts(list(artifact_to_route_pcr_free), stage_uri=stage.uri)
+            self.lims.route_artifacts(list(artifact_to_route_userprepared), stage_uri=stage.uri)
 
         if artifact_to_route_preseqlab:
             # Only route artifacts if there are any artifacts to go to Nano
             stage = get_workflow_stage(self.lims, "PreSeqLab EG 6.0", "Spectramax Picogreen EG 6.0")
-            self.lims.route_artifacts(list(artifact_to_route_nano), stage_uri=stage.uri)
+            self.lims.route_artifacts(list(artifact_to_route_preseqlab), stage_uri=stage.uri)
 
 
 
