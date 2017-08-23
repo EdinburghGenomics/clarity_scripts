eac82c82412efd760f32fb7ed057e3722ec0c489
dforth <dan.forth@ed.ac.uk>
2017-08-23 15:59:38 +0100

    New script for naming containers

--- scripts/assign_container_name.py
+++ scripts/assign_container_name.py
@@ -4,18 +4,14 @@ from EPPs.common import StepEPP, step_argparser, get_workflow_stage
 
 class AssignContainerName(StepEPP):
 
-    def nameContainer(self, projectName):
-        artifacts = self.process.all_outputs(unique=True)
-        artifacts[0].container.name = projectName
-        artifacts[0].container.name.put()
-
-
-
     def _run(self):
         #projects = self.projects(unique=True)
         #projectName=projects[0].name
         #print(projectName)
-        nameContainer('test2017-08-23')
+        artifacts = self.process.all_outputs(unique=True)
+        artifacts[0].container.name = 'test'
+        artifacts[0].container.name.put()
+
 
 
 
