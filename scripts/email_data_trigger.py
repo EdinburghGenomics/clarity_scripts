1ec484f9fa69d7c44bbf1bb1cf222aeb1795d1d5
dforth <dan.forth@ed.ac.uk>
2017-10-04 17:02:48 +0100

    Adding data_download_contacts

--- scripts/email_data_trigger.py
+++ scripts/email_data_trigger.py
@@ -15,9 +15,9 @@ class DataReleaseEmailAndUpdateEPP(SendMailEPP):
 
         # Create a list of project description like <project name>: <number of sample> sample(s)
         project_list = ['%s: %s sample(s)' % (len(sample_per_project[p]), p) for p in sample_per_project]
-
+        data_download_contact1= process.udf.get("Data Download Contact Name 1")
         # Create the message
-        msg = 'Hi Bioinformatics,\n\nPlease release the data for the samples shown at the link below to the Data Contacts also shown at the link:\n\n{link}\n\nKind regards,\nClarityX'
+        msg = 'Hi Bioinformatics,'+data_download_contact1+'\n\nA data release is required. See the link below for the samples to be released and the data contact names:\n\n{link}\n\nKind regards,\nClarityX'
 
         # fill in message with parameters
         msg = msg.format(
