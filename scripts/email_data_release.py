3b1fab35a8277b24846be38ba60f0c5166c6bde0
dforth <dan.forth@ed.ac.uk>
2018-01-24 14:42:17 +0000

    Updating link

---
+++ scripts/email_data_release.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+from collections import defaultdict
+import platform
+from EPPs.common import step_argparser, SendMailEPP
+from EPPs.config import load_config
+
+
+class DataReleaseEmail(SendMailEPP):
+
+    def _run(self):
+        if len(self.projects)>1: #check if more than one project in step, only one permitted
+            raise ValueError('More than one project present in step. Only one project per step permitted')
+
+
+
+        # Create the message
+
+
+        msg = 'Hi,\n\n Data for {sample_count} sample(s) has been released for {project} at:\n\n{link}\n\nKind regards,\nClarityX'
+
+        # fill in message with parameters
+        msg = msg.format(
+            link='https://'+platform.node() + '/clarity/work-details/' + self.step_id[3:],
+            sample_count=len(self.samples),
+            project=self.samples[0].project.name,
+        )
+        subject = ', '.join([p.name for p in self.projects]) + ': Data Released'
+
+        # Send email to list of persons specified in the default section of config
+        self.send_mail(subject, msg)
+
+        # Alternatively You can send the email to specific section of config
+        #self.send_mail(subject, msg, config_name='project_only')
+
+        if self.process.UDF.get("Request Customer Survey (Final Data Release)") = "Yes":
+            msg2 = 'Hi,\n\n The final data release has occurred for {project}. Please request a customer survey.\n\nKind regards,\nClarityX'
+
+            # fill in message with parameters
+            msg2 = msg.format(
+                link='https://'+platform.node() + '/clarity/work-details/' + self.step_id[3:],
+                sample_count=len(self.samples),
+                project=self.samples[0].project.name,
+            )
+            subject2 = ', '.join([p.name for p in self.projects]) + ': Request Customer Survey - Final Data Released'
+            self.send_mail(subject2, msg2)
+
+def main():
+    # Ge the default command line options
+    p = step_argparser()
+
+    # Parse command line options
+    args = p.parse_args()
+
+    # Load the config from the default location
+    load_config()
+
+    # Setup the EPP
+    action = DataReleaseEmail(
+        args.step_uri, args.username, args.password, args.log_file,
+    )
+
+    # Run the EPP
+    action.run()
+
+
+if __name__ == "__main__":
+    main()
