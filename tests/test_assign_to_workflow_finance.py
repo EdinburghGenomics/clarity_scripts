1850a50f7f81994fefb03cf9a18265310ed7ab93
dforth <dan.forth@ed.ac.uk>
2018-04-20 18:03:25 +0100

    Corrected tests (test_check_step_UDFs does not check that exit is triggered when udf is empty)

--- tests/test_assign_to_workflow_finance.py
+++ tests/test_assign_to_workflow_finance.py
@@ -3,11 +3,11 @@ from tests.test_common import TestEPP, fake_artifact
 from unittest.mock import Mock, patch, PropertyMock
 
 
-def fake_all_outputs(unique=False, resolve=False):
+def fake_all_inputs(unique=False, resolve=False):
     '''Return a list of mocked artifacts which contain sample which contain artifact ... Simple!'''
     return (
         Mock(samples=[Mock(artifact=fake_artifact('a1'), id='s1')]),
-        Mock(samples=[Mock(artifact=fake_artifact('a2'), id='s2')]),
+        Mock(samples=[Mock(artifact=fake_artifact('a2'), id='s2')])
 
     )
 
@@ -17,11 +17,11 @@ class TestAssignWorkflowSampleReview(TestEPP):
         self.patched_process = patch.object(
             AssignWorkflowFinance,
             'process',
-            new_callable=PropertyMock(return_value=Mock(all_outputs=fake_all_outputs))
+            new_callable=PropertyMock(return_value=Mock(all_inputs=fake_all_inputs))
         )
         self.patched_lims = patch.object(AssignWorkflowFinance, 'lims', new_callable=PropertyMock)
         self.patched_get_workflow_stage = patch(
-            'scripts.assign_sample_review.get_workflow_stage',
+            'scripts.assign_workflow_finance.get_workflow_stage',
             return_value=Mock(uri='a_uri')
         )
 
@@ -41,4 +41,3 @@ class TestAssignWorkflowSampleReview(TestEPP):
             assert observed == ['a1','a2']
             assert self.epp.lims.route_artifacts.call_args[1] == {'stage_uri': 'a_uri'}
 
-            #assert self.epp.lims.route_artifacts.call_args[1] != {'stage_uri': 'a_uri'}
