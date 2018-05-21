7260a261341111b0f3ccee96ece2bf3bcf4325b6
dforth <dan.forth@ed.ac.uk>
2018-05-21 11:44:33 +0100

    Updated so all User Prepared Library samples go to Nano. Test updated also.

--- tests/test_assign_to_workflow_user_library.py
+++ tests/test_assign_to_workflow_user_library.py
@@ -6,8 +6,7 @@ from unittest.mock import Mock, patch, PropertyMock, call
 def fake_all_output(unique=False, resolve=False):
     """Return a list of mocked artifacts which contain samples which contain artifacts... Simple!"""
     return (
-        Mock(id='ao1', samples=[Mock(artifact=fake_artifact('a1'), id='s1', udf={'Prep Workflow': 'TruSeq PCR-Free DNA Sample Prep'})]),
-        Mock(id='ao2', samples=[Mock(artifact=fake_artifact('a2'), id='s2', udf={'Prep Workflow': 'TruSeq Nano DNA Sample Prep'})]),
+        Mock(id='ao1'),
     )
 
 
@@ -36,14 +35,9 @@ class TestAssignWorkflowUserPreparedLibrary(TestEPP):
             self.epp._run()
 
             pws.assert_has_calls((
-                call(self.epp.lims, 'TruSeq PCR-Free DNA Sample Prep', 'SEMI-AUTOMATED - Make and Read qPCR Quant'),
-                call(self.epp.lims, 'TruSeq Nano DNA Sample Prep', 'SEMI-AUTOMATED - Make LQC & Caliper GX QC'),
+                call(self.epp.lims, 'TruSeq Nano DNA Sample Prep', 'SEMI-AUTOMATED - Make and Read qPCR Quant'),
             ))
-            # first routing (pcr free)
+            # first routing (first artifact in list, should route to nano)
             route_args = self.epp.lims.route_artifacts.call_args_list[0]
             assert sorted([a.id for a in route_args[0][0]]) == ['ao1']
             assert self.epp.lims.route_artifacts.call_args[1] == {'stage_uri': 'a_uri'}
-
-            # second routing (nano)
-            route_args = self.epp.lims.route_artifacts.call_args_list[1]
-            assert sorted([a.id for a in route_args[0][0]]) == ['ao2']
