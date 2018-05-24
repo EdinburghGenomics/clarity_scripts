e7bf1b140edf18e75748a2b79d7918abdf08a879
dforth <dan.forth@ed.ac.uk>
2018-05-24 15:44:24 +0100

    New script for assigning the next in User Prepared Library Plate receipt based on the answers to UDFs completed by the lab

---
+++ scripts/next_step_assignment_user_prepared_plate_receipt.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+from EPPs.common import step_argparser, StepEPP
+
+
+class AssignNextStepQuantStudio(StepEPP):
+    """
+    This script assigns the next step for samples in the QuantStudio Data Import step. It assigns the next step as
+    either complete or review depending on whether the number of SNP calls exceeds the minimum number of calls.
+    """
+
+    def _run(self):
+
+        # obtain the actions of the step then creates a StepActions entity for the current step
+        actions = self.process.step.actions
+
+        # obtain the next actions in the step then creates a list of dict for next_actions for the step for all artifacts
+        next_actions = actions.next_actions
+
+        # check step UDFs answered by lab team to confirm that sample is in good condition. If UDFs both answered "Yes" then
+        #set next action as the next step, if one or more UDF is set to "No" then set as "review". The next step in the protocol
+        #is found rather than hard coded.
+
+        for next_action in next_actions:
+
+            if self.process.udf.get("Plate(s) Undamaged and Sealed?") =='No':
+                next_action['action'] = 'review'
+            elif self.process.udf.get("Samples Present and Frozen in Wells?") == 'No':
+                    next_action['action'] = 'review'
+
+            else:
+                current_step = self.process.step.configuration  # configuration gives the ProtocolStep entity.
+                protocol = Protocol(self.process.lims,
+                                    uri='/'.join(self.process.step.configuration.uri.split('/')[:-2]))
+                steps = protocol.steps  # a list of all the ProtocolSteps in protocol
+                step_object = steps[steps.index(current_step) + 1]
+                next_action['action'] = 'nextstep'
+                next_action['step'] = step_object
+
+        actions.put()
+
+
+def main():
+    # Get the default command line options
+    p = step_argparser()
+
+    # Parse command line options
+    args = p.parse_args()
+
+    # Setup the EPP
+    action = AssignNextStepQuantStudio(
+        args.step_uri, args.username, args.password, args.log_file
+    )
+
+    # Run the EPP
+    action.run()
+
+
+if __name__ == "__main__":
+    main()
