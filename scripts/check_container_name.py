393ca1eb73f227c7377180c3e6cac86296dc2479
dforth <dan.forth@ed.ac.uk>
2018-05-29 09:54:28 +0100

    New script for checking the format of container names assigned by the lab

---
+++ scripts/check_container_name.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import re
+
+from EPPs.common import StepEPP, step_argparser
+
+
+class CheckContainerName(StepEPP):
+    """
+    Checks that the container name assigned by the user has the correct format for a seqlab plate with the standard
+    seqlab prefix "LP[0-9]{7}-" and the suffix specified by an argument.
+    """
+
+    def __init__(self, step_uri, username, password, suffix):
+        #extra suffix argument required in addition to standard arg parser arguments
+        super().__init__(step_uri, username, password)
+        self.suffix = suffix
+
+    def _run(self):
+        """
+        Assembles the container name template from the fixed prefix and the suffix determined by the argument.
+        Find all the output container names and then check that they match the template. If not then sys exit with
+        a useful message.
+        """
+        name_template = "LP[0-9]{7}-" + suffix
+        containers = self.process.output_containers()
+
+        for container in containers:
+            if not re.match(name_template, container):
+                print("%s is not a valid container name. Container names must match %s" % (container, name_template))
+                sys.exit(1)
+
+
+def main():
+    p = step_argparser()
+    args = p.parse_args()
+    p.add_argument('-x', '--suffix', type=str, help='set the suffix of the container name (hyphen present in prefix)',
+                   default=False)
+    action = CheckContainerName(args.step_uri, args.username, args.password, args.suffix)
+    action.run()
+
+
+if __name__ == '__main__':
+    main()
