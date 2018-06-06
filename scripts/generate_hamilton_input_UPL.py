f13ea223ec5e9d882ff8871848f47ae136230e39
dforth <dan.forth@ed.ac.uk>
2018-06-06 15:47:49 +0100

    New script for generating a Hamilton output

--- scripts/generate_hamilton_input_UPL.py
+++ scripts/generate_hamilton_input_UPL.py
@@ -10,7 +10,7 @@ class GenerateHamiltonInputUPL(StepEPP):
     def _run(self):
         all_inputs = self.process.all_inputs()
         csv_array=[]
-        csv_column_headers=['\nInput Plate,','Input Well','Output Plate','Output Well,','DNA Volume,','TE Volume']
+        csv_column_headers=['Input Plate,','Input Well','Output Plate','Output Well,','DNA Volume,','TE Volume']
         csv_array.append(csv_column_headers)
 
 
@@ -19,16 +19,16 @@ class GenerateHamiltonInputUPL(StepEPP):
                 output = self.process.outputs_per_input(input.id,Analyte=True)
                 if output[0].type =='Analyte':
                     output_container=output[0].container.name
-                    output_well=output[0].location
+                    output_well=output[0].location[1]
 
 
-                csv_line=[input.container.name,input.location,output_container,output_well,self.process.udf['DNA Volume (uL)'],'0']
+                csv_line=[input.container.name,input.location[1],output_container,output_well,self.process.udf['DNA Volume (uL)'],'0']
                 csv_array.append(csv_line)
         print(csv_array)
 
         with open('hamilton_input.csv','w') as csvFile:
             writer = csv.writer(csvFile)
-            writer.writerows(csvData)
+            writer.writerows(csv_array)
         csvFile.close()
 
 def main():
