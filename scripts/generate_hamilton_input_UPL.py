83e8066f3fa1ec63be71999c9a443e65a26e410b
dforth <dan.forth@ed.ac.uk>
2018-06-06 17:01:52 +0100

    New script for generating a Hamilton output

--- scripts/generate_hamilton_input_UPL.py
+++ scripts/generate_hamilton_input_UPL.py
@@ -15,20 +15,30 @@ class GenerateHamiltonInputUPL(StepEPP):
 
     def _run(self):
         all_inputs = self.process.all_inputs()
+        csv_dict={}
         csv_array = []
         csv_column_headers = ['Input Plate', 'Input Well', 'Output Plate', 'Output Well', 'DNA Volume', 'TE Volume']
         csv_array.append(csv_column_headers)
 
-        for input in sorted(all_inputs):
+        for input in all_inputs:
             if input.type == 'Analyte':
                 output = self.process.outputs_per_input(input.id, Analyte=True)
-                if output[0].type == 'Analyte':
-                    output_container = output[0].container.name
-                    output_well = output[0].location[1]
+                output_container = output[0].container.name
+                output_well = output[0].location[1]
 
                 csv_line = [input.container.name, input.location[1], output_container, output_well,
                             self.process.udf['DNA Volume (uL)'], '0']
-                csv_array.append(csv_line)
+                csv_dict[input.location[1]]=csv_line
+                #csv_array.append(csv_line)
+        print(csv_dict)
+
+        rows=['A','B','C','D','E','F','G','H']
+        columns=['1','2','3','4','5','6','7','8','9','10','11','12']
+
+        for row in rows:
+            for column in columns:
+                if csv_dict[row+":"+column]:
+                    csv_array.append(csv_dict[row+":"+column])
 
         file_name = self.hamilton_input + '-hamilton_input.csv'
 
