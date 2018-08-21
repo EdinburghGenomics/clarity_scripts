80fe5abec280308834505b37ec6ce01cfb76258a
dforth <dan.forth@ed.ac.uk>
2018-08-21 13:18:45 +0100

    New scripts for KAPA Library prep: 1. Generate Hamilton Input for Picogreen 2. QC Check - performs a logical comparison output and step UDFs provided by arguments with operator and the "qc result" if a fail being provided by the argumnets also. 3. Spectramax Seq Plate Quant - parses the spectramax file for 3 replicates and adds the mean to the input artifact 4. subtraction - used to track volume changes in wells in steps 5. Next Step Assignment Seq Pico - assigns next step as either review (if fail) or complete 6. Normalisation - for calculating the volume of sample and buffer required for normalisation 7. Generate Hamilton Input for CFP 8. Generate Hamilton Input for IMP 9. Generate Hamilton Input for UCT

--- scripts/generate_hamilton_input_QPCR_layout.py
+++ scripts/generate_hamilton_input_QPCR_layout.py
@@ -2,6 +2,8 @@
 import csv
 import re
 import sys
+from operator import itemgetter
+
 
 from EPPs.common import StepEPP, step_argparser
 
@@ -16,9 +18,11 @@ class GenerateHamiltonInputUPL(StepEPP):
         self.hamilton_input = hamilton_input
 
     def _run(self):
-        # csv_dict will be a dictionary that consists of the lines to be present in the Hamilton input file. These are
+        # libraries_csv_dict_dict will be a dictionary that consists of the lines to be present in the Hamilton input file for the input libraries.
+        # standards_csv_dict will be a dictionary that consists of the lines to be present in the Hamilton input file for the standards. These are
         # then sorted into correct order and added to the csv_array which is used to write the file
-        csv_dict = {}
+        libraries_csv_dict = {}
+        standards_csv_dict = {}
         csv_array = []
 
         # define the column headers that will be used in the Hamilton input file and add to the csv_array to be
@@ -68,6 +72,7 @@ class GenerateHamiltonInputUPL(StepEPP):
         # find all the inputs for the step that are analytes (i.e. samples and not associated files) then add them to a set
         for input in all_inputs:
             if input.type == 'Analyte':
+
                 input_analytes.add(input)
 
                 output = self.process.outputs_per_input(input.id, ResultFile=True)
@@ -86,43 +91,78 @@ class GenerateHamiltonInputUPL(StepEPP):
 
                 unique_output_containers.add(output[0].container.name)
 
-                # assemble each line of the Hamilton input file in the correct structure for the Hamilton
+                # output locations i.e. wells in the QPCR plate need to appear in numerical order so they appear in column order
+                # in the QPCR well-X columns.
+                output_locations = []
+                counter = 0
+                #there are 3 replicates so 3 output locations, capture the row and column for each location in a list of lists
+                while counter <= 2:
+                    row,column=output[counter].location[1].split(":")
+                    output_locations.append([row,int(column)])
+                    counter = counter+1
 
-                csv_line = [input.container.name, input.location[1], output[0].container.name, output[0].location[1],
-                            output[1].location[1], output[2].location[1], DIL1_barcode, DIL2_barcode, QSTD_barcode,
-                            QMX_barcode]
+
+                #use 'sorted' and 'itemgetter' to sort the output locations first by the row then by the column
+                output_locations=sorted(output_locations, key=itemgetter(0,1))
+
+                #reset count to 0
+                counter = 0
+
+                #reassemble the well location strings as row:column
+                output_locations_final=[]
+                while counter <= 2:
+                    output_locations_final.append(output_locations[counter][0]+":"+str(output_locations[counter][1]))
+                    counter = counter+1
+
+
+                # assemble each line of the Hamilton input file in the correct structure for the Hamilton
+                ##csv_line = [input.container.name, input.location[1], output[0].container.name, output[0].location[1],
+                ## output[1].location[1], output[2].location[1], DIL1_barcode, DIL2_barcode, QSTD_barcode,
+                ##   QMX_barcode]
+
+                csv_line = [input.container.name, input.location[1], output[0].container.name,
+                            output_locations_final[0], output_locations_final[1], output_locations_final[2],
+                            DIL1_barcode, DIL2_barcode, QSTD_barcode,QMX_barcode]
 
                 # build a dictionary of the lines for the Hamilton input file with a key that facilitates the lines being
                 # by input container then column then row
-                csv_dict[input.location[1]] = csv_line
+                if input.location[1] == "1:1":
+                    standards_csv_dict[input.name] = csv_line
+                else:
+                    libraries_csv_dict[input.location[1]] = csv_line
 
         # check the number of inputs
         if len(input_analytes) > 3:
             print('Maximum number of input samples is 23. There are %s input samples in the step.' % (
                 str(len(input_analytes))))
-            #sys.exit(1)
+            # sys.exit(1)
 
         # check the number of input containers
         if len(unique_input_containers) > 1:
             print('Maximum number of input plates is 1. There are %s output plates in the step.' % (
                 str(len(unique_input_containers))))
-            #sys.exit(1)
+            # sys.exit(1)
 
         # check the number of output containers
         if len(unique_output_containers) > 1:
             print('Maximum number of output plates is 1. There are %s output plates in the step.' % (
                 str(len(unique_output_containers))))
-            #sys.exit(1)
+            # sys.exit(1)
 
-        # define the rows and columns in the input plate (standard 96 well plate pattern)
+        # define the rows and columns in the library input plate (standard 96 well plate pattern)
         rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
         columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 
-        # add the lines to the csv_array that will be used to write the Hamilton input file
+        # add the lines to the csv_array that will be used to write the Hamilton input file for the input libraries
         for column in columns:
             for row in rows:
-                if row + ":" + column in csv_dict.keys():
-                    csv_array.append(csv_dict[row + ":" + column])
+                if row + ":" + column in libraries_csv_dict.keys():
+                    csv_array.append(libraries_csv_dict[row + ":" + column])
+
+        # standards do not have plate and well information in the LIMS. Append them to the end of the csv_array in alphanumeric order
+        # of sample name
+        for standard in sorted(standards_csv_dict):
+            csv_array.append(standards_csv_dict[standard])
 
         # create and write the Hamilton input file, this must have the hamilton_input argument as the prefix as this is used by
         # Clarity LIMS to recognise the file and attach it to the step
