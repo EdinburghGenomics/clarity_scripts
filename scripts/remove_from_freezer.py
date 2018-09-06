fd21f5fe473e67a2aa1870def1dc102490fb323e
dforth <dan.forth@ed.ac.uk>
2018-09-06 13:19:11 +0100

    Updated to compare ids rather than variable

--- scripts/remove_from_freezer.py
+++ scripts/remove_from_freezer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# !/usr/bin/env python
-from EPPs.common import step_argparser, StepEPP
+import sys
+from EPPs.common import StepEPP
 
 
 # the freezer location of the sample entering the step should be updated to match the step UDFs. The script checks
@@ -11,35 +11,20 @@ class UpdateFreezerLocation(StepEPP):
     def _run(self):
         # process each artifact in the step
         for artifact in self.artifacts:
+
             # if the sample is a submitted sample then the artifact in the step will match the artifact obtained if
             # we obtain the submitted sample and then its equivalent artifact
-            if str(artifact) == str(artifact.samples[0].artifact):
+            if artifact.id == artifact.samples[0].artifact.id:
                 artifact.samples[0].udf['Freezer'] = self.process.udf.get('New Freezer Location')
                 artifact.samples[0].udf['Shelf'] = self.process.udf.get('New Freezer Location')
                 artifact.samples[0].put()
             # if the sample is a derived sample then the artifact in the step will not match the artifact obtained if
             # we obtain the submitted sample and then its equivalent artifact
-            elif str(artifact) != str(artifact.samples[0].artifact):
+            elif artifact.id != artifact.samples[0].artifact.id:
                 artifact.udf['Freezer'] = self.process.udf.get('New Freezer Location')
                 artifact.udf['Shelf'] = self.process.udf.get('New Freezer Location')
                 artifact.put()
 
 
-def main():
-    # Get the default command line options
-    p = step_argparser()
-
-    # Parse command line options
-    args = p.parse_args()
-
-    # Setup the EPP
-    action = UpdateFreezerLocation(
-        args.step_uri, args.username, args.password, args.log_file,
-    )
-
-    # Run the EPP
-    action.run()
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    sys.exit(UpdateFreezerLocation().run())
