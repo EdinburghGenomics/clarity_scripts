65cac97979193490b3e656f206bc457fddcd5d5b
dforth <dan.forth@ed.ac.uk>
2018-10-12 15:42:26 +0100

    updated test to match changes in original script

--- tests/test_autoplacement_24_in_96.py
+++ tests/test_autoplacement_24_in_96.py
@@ -1,35 +1,24 @@
 from unittest.mock import Mock, patch, PropertyMock
 
-from scripts.autoplacement_24_in_96 import AutoplacementMakeCFP
+from scripts.autoplacement_24_in_96 import Autoplacement24in96
 from tests.test_common import TestEPP
 
-
-#def fake_outputs_per_input(inputid, Analyte=True):
- #   fake_outputs={
-  #      'ai1': [Mock(id='ao1', container='Container1', location='')],
-   #     'ai2': [Mock(id='ao2', container='Container1', location='')]
-    #}
-    #return (
-     #   fake_outputs[inputid]
-    #)
-
-fake_selected_containers=[Mock(id='c1')]
-fake_outputs_per_input=[Mock(id='ao1', container='Container1', location='')]
+fake_selected_containers = [Mock(id='c1')]
+fake_outputs_per_input = [Mock(id='ao1', container='Container1', location='')]
 
 
-class TestAutoplacementMakeCFP(TestEPP):
+class TestAutoplacement24in96(TestEPP):
     def setUp(self):
-
         fake_inputs = [
-            Mock(id='ai1', type='Analyte',output=[Mock(id='ao1', type='Analyte', container='Container1', location='')]),
-            Mock(id='ai2', type='Analyte', output=[Mock(id='ao2', type='Analyte', container='Container1', location='')])
+            Mock(id='ai1', type='Analyte', container='container', location=('container', 'A:1'),
+                 output=[Mock(id='ao1', type='Analyte', container='Container1', location=('container', ''))]),
+            Mock(id='ai2', type='Analyte', container='container', location=('container', 'B:1'),
+                 output=[Mock(id='ao2', type='Analyte', container='Container1', location=('container', ''))])
 
         ]
 
-
-
         self.patched_process1 = patch.object(
-            AutoplacementMakeCFP,
+            Autoplacement24in96,
             'process',
             new_callable=PropertyMock(
                 return_value=Mock(
@@ -43,19 +32,19 @@ class TestAutoplacementMakeCFP(TestEPP):
             )
         )
 
-        self.patched_lims = patch.object(AutoplacementMakeCFP, 'lims', new_callable=PropertyMock)
+        self.patched_lims = patch.object(Autoplacement24in96, 'lims', new_callable=PropertyMock)
 
-        self.epp = AutoplacementMakeCFP(
+        self.epp = Autoplacement24in96(
             self.default_argv)
 
-    def test_automated_placement_make_cfp(self):
+    def test_autoplacement_24_in_96(self):
         # per input
         with self.patched_process1, self.patched_lims:
             self.epp._run()
 
             expected_output_placement = [
                 (fake_outputs_per_input[0], (fake_selected_containers[0], 'A:1')),
-                (fake_outputs_per_input[0], (fake_selected_containers[0], 'B:1' ))
+                (fake_outputs_per_input[0], (fake_selected_containers[0], 'B:1'))
             ]
             self.epp.process.step.set_placements.assert_called_with(fake_selected_containers, expected_output_placement)
-            #assert self.epp.process.step.get_placement_list == ['']
+            # assert self.epp.process.step.get_placement_list == ['']
