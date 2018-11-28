43fa27ba2e5416e8d6204c935d86b61b229927b1
dforth <dan.forth@ed.ac.uk>
2018-11-28 16:05:16 +0000

    As per previous commit comment

--- scripts/generate_hamilton_input_imp_ssqc.py
+++ scripts/generate_hamilton_input_imp_ssqc.py
@@ -4,21 +4,21 @@ import sys
 from EPPs.common import GenerateHamiltonInputEPP, InvalidStepError
 
 
-class GenerateHamiltonInputIMP(GenerateHamiltonInputEPP):
+class GenerateHamiltonInputIMPSSQC(GenerateHamiltonInputEPP):
     """"Generate a CSV containing the necessary information to transfer the fragmented sample from a covaris plate into
     an IMP plate"""
     _use_load_config = False  # prevent the loading of the config
     # Define the column headers that will be used in the Hamilton input file
-    csv_column_headers = ['Input Plate', 'Input Well', 'Output Plate', 'Output Well', 'CFP Volume']
+    csv_column_headers = ['Input Plate', 'Input Well', 'Output IMP Plate', 'Output IMP Well', 'CFP to IMP Volume','Output SSQC Plate', 'Output SSQC Well' 'CFP to SSQC Volume','RSB to SSQC Volume']
 
     # Define the output file
-    output_file_name = 'KAPA_TRANSFER_CFP_TO_IMP.csv'
+    output_file_name = 'KAPA_TRANSFER_CFP_TO_IMP_AND_SSQC.csv'
 
     # Define the number of input containers that are permitted
     permitted_input_containers = 1
 
     # Define the number of output containers that are permitted
-    permitted_output_containers = 1
+    permitted_output_containers = 2
 
     def _generate_csv_dict(self):
 
@@ -26,7 +26,9 @@ class GenerateHamiltonInputIMP(GenerateHamiltonInputEPP):
         csv_dict = {}
 
         # obtain the cfp volume from the step udf
-        cfp_volume = self.process.udf['CFP Volume (ul)']
+        cfp_imp_volume = self.process.udf['CFP to IMP Volume (ul)']
+        cfp_SSQC_volume = self.process.udf['CFP to SSQC Volume (ul)']
+        rsb_ssqc_volume = self.process.udf['RSB to SSQC Volume (ul)']
 
         # find all the inputs for the step that are analytes (i.e. samples and not associated files)
         for input_art in self.artifacts:
@@ -34,22 +36,22 @@ class GenerateHamiltonInputIMP(GenerateHamiltonInputEPP):
                 outputs = self.process.outputs_per_input(input_art.id, Analyte=True)
 
                 # the script is only compatible with 1 output for each input i.e. replicates are not allowed
-                if len(outputs) > 1:
+                if len(outputs) != 2:
                     raise InvalidStepError(
-                        message='Multiple outputs found for an input %s. This step is not compatible with replicates.' % (
+                        message='Incorrect number of outputs found for %s. This step requires two outputs per input.' % (
                             input_art.name))
-
-                # only 1 output expected so output is always position 0 in the outputs list
-                output = outputs[0]
 
                 # remove semi-colon from locations as this is not compatible with Hamilton Venus software
                 input_location = input_art.location[1].replace(':', '')
-                output_location = output.location[1].replace(':', '')
+                output_locations = []
+
+                for output in outputs:
+                    output_locations.append(output.location[1].replace(':', ''))
 
                 # assemble each line of the Hamilton input file in the correct structure for the Hamilton
 
-                csv_line = [input_art.container.name, input_location, output.container.name, output_location,
-                            cfp_volume]
+                csv_line = [input_art.container.name, input_location, outputs[0].container.name, output_locations[0],
+                            cfp_imp_volume,outputs[1].container.name, output_locations[1],cfp_SSQC_volume,rsb_ssqc_volume]
 
                 # build a dictionary of the lines for the Hamilton input file with a key that facilitates the lines being
                 # by input container then column then row
@@ -59,4 +61,4 @@ class GenerateHamiltonInputIMP(GenerateHamiltonInputEPP):
 
 
 if __name__ == '__main__':
-    sys.exit(GenerateHamiltonInputIMP().run())
+    sys.exit(GenerateHamiltonInputIMPSSQC().run())
