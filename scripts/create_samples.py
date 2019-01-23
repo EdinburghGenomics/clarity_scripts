2926c90a35acc10bbe7fc23198aa23225722a798
dforth <dan.forth@ed.ac.uk>
2019-01-23 17:34:22 +0000

    updated

--- scripts/create_samples.py
+++ scripts/create_samples.py
@@ -107,7 +107,7 @@ class CreateSamples(StepEPP):
                 'Stage specified by workflow: %s and stage: %s does not exist in %s' % (
                     self.process.udf['Next Workflow'], self.process.udf['Next Step'], self.baseuri)
             )
-
+        self.lims.route_artifacts(samples_to_update, stage_uri=stage.uri)
 
 if __name__ == "__main__":
     CreateSamples().run()
