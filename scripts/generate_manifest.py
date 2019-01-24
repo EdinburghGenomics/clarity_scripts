f75c70231c5254a8ee6bf4e38a22dc47ea473fbc
dforth <dan.forth@ed.ac.uk>
2019-01-24 16:25:51 +0000

    Updated to send email message

--- scripts/generate_manifest.py
+++ scripts/generate_manifest.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+import itertools
 from openpyxl import load_workbook
+import os
 
 
-from EPPs.common import StepEPP
+from EPPs.common import SendMailEPP
 
-class GenerateManifest96WellPlate(StepEPP):
+
+class GenerateManifest96WellPlate(SendMailEPP):
     # populate the sample manifest with the sample date. Sample manifest template is determined by a step udf.
     # The starting row and columns are determined by step UDFs.
 
-    _use_load_config = False  # prevent the loading of the config file
+    _use_load_config = True  # prevent the loading of the config file
 
     # additional argument required to obtain the file location for newly create manifest in the LIMS step
     def __init__(self, argv=None):
@@ -26,11 +29,14 @@ class GenerateManifest96WellPlate(StepEPP):
         # obtain all of the inputs for the step
         all_inputs = self.process.all_inputs(unique=True)
 
-        # check all input containers have the same type
+        # check all input containers have the same type and also create list of unique containers
         container_types = set()
+        unique_containers=set()
+
 
         for artifact in all_inputs:
             container_types.add(artifact.container.type)
+            unique_containers.add(artifact.container)
 
         if len(container_types) > 1:
             raise ValueError('Only 1 container type is permitted. Multiple container types are present in the step')
@@ -70,33 +76,62 @@ class GenerateManifest96WellPlate(StepEPP):
             sample_dict[artifact.location[1].replace(":","")]=artifact.samples[0]
 
 
-        for column in columns:
-            for row in rows:
-                # populate the manifest with sample attributes
-                sample_udf=sample_dict[row+column].udf
-                #sample_udf = artifact.samples[0].udf
-                # populate the wells in the active work sheet in the excel in the columns defined by the step UDFs
-                if con_type == '[Plates]':
-                    ws[step_udfs[con_type + 'Sample Name'] + str(row_counter)] = sample_dict[row+column].artifact.name
-                    ws[step_udfs['[Plates]Container Name'] + str(row_counter)] = sample_dict[row+column].artifact.container.name
-                    ws[step_udfs['[Plates]Well'] + str(row_counter)] = sample_dict[row+column].artifact.location[1]
-                    ws[step_udfs['[Plates]Project ID'] + str(row_counter)] =sample_dict[row+column].project.name
+        for column,row in itertools.product(columns,rows):
+            if row+column not in sample_dict:
+                continue
+            #if condition met then 'continue' means will skip back to beginning of loop
 
-                if con_type == '[Tubes]':
-                    ws[step_udfs['[Tubes]Project ID Well']] = sample_dict[row+column].project.name
+            # populate the manifest with sample attributes
+            sample_udf=sample_dict[row+column].udf
+            #sample_udf = artifact.samples[0].udf
+            # populate the wells in the active work sheet in the excel in the columns defined by the step UDFs
+            if con_type == '[Plates]':
+                ws[step_udfs[con_type + 'Sample Name'] + str(row_counter)] = sample_dict[row+column].artifact.name
+                ws[step_udfs['[Plates]Container Name'] + str(row_counter)] = sample_dict[row+column].artifact.container.name
+                ws[step_udfs['[Plates]Well'] + str(row_counter)] = sample_dict[row+column].artifact.location[1]
+                ws[step_udfs['[Plates]Project ID'] + str(row_counter)] =sample_dict[row+column].project.name
 
-                # populate the manifest with sample UDFs which are configurable by adding or removing step UDFs in the
-                # format [CONTAINER TYPE - either Tubes or Plates][Sample UDF]Name of UDF
-                for configurable_udf in configurable_udfs:
-                    if con_type + '[Sample UDF]' + configurable_udf in step_udfs.keys():
-                        ws[step_udfs[con_type + '[Sample UDF]' + configurable_udf] + str(row_counter)] = \
-                            sample_udf[configurable_udf]
+            if con_type == '[Tubes]':
+                ws[step_udfs['[Tubes]Project ID Well']] = sample_dict[row+column].project.name
 
-                row_counter += 1
+            # populate the manifest with sample UDFs which are configurable by adding or removing step UDFs in the
+            # format [CONTAINER TYPE - either Tubes or Plates][Sample UDF]Name of UDF
+            for configurable_udf in configurable_udfs:
+                if con_type + '[Sample UDF]' + configurable_udf in step_udfs.keys():
+                    ws[step_udfs[con_type + '[Sample UDF]' + configurable_udf] + str(row_counter)] = \
+                        sample_udf[configurable_udf]
+
+            row_counter += 1
+
+
 
         # create a new file with the original file name plus a suffix containing the project ID
-        new_filepath = self.manifest + '-'+'Edinburgh_Genomics_Sample_Submission_Manifest_' + all_inputs[0].samples[0].project.name + '.xlsx'
-        wb.save(filename=new_filepath)
+        lims_filepath = self.manifest + '-'+'Edinburgh_Genomics_Sample_Submission_Manifest_' + all_inputs[0].samples[0].project.name + '.xlsx'
+        wb.save(filename=lims_filepath)
+        #write a copy of the file without the LIMS prefix that can be attached to a customer email
+        email_filepath='Edinburgh_Genomics_Sample_Submission_Manifest_' + all_inputs[0].samples[0].project.name + '.xlsx'
+        wb = load_workbook(filename=lims_filepath)
+        wb.save(filename=email_filepath)
+
+        #send an email to the project manager using customer manifest template with the new manifest attached
+        project_name = all_inputs[0].samples[0].project.name
+        #choose a species for the email subject, this can be updated manually by the project manager
+        species=all_inputs[0].samples[0].udf['Species']
+        email_subject= project_name+": "+species+" WGS Sample Submission"
+
+        #the manifest and relevant requirements document for the container type should be attached to the email.
+        attachments_list=[]
+        attachments_list.append(email_filepath)
+        if container_types[0].name='96 well plate':
+            attachments_list.append(self.process.udf['Plates Requirements Path'])
+        elif container_types[0].name='rack 96 positions':
+            attachments_list.append(self.process.udf['Tubes Requirements Path'])
+
+        self.send_mail(email_subject,None, project=project_name,template_name='customer_manifest.html',
+                       config_name='projects-facility-finance_only', attachments=[email_filepath])
+        #delete the copy of the manifest that was attached to the email
+        os.remove(email_filepath)
+
 
 
 if __name__ == '__main__':
