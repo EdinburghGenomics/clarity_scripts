5df1741139dad5017a2426b182e6362ac9701a08
dforth <dan.forth@ed.ac.uk>
2019-01-30 09:54:29 +0000

    Updated so sorts samples by container (as well as row and column) before writing to manifest which means the different groups of samples appear together

--- scripts/generate_manifest.py
+++ scripts/generate_manifest.py
@@ -28,15 +28,20 @@ class GenerateManifest96WellPlate(SendMailEPP):
 
         # obtain all of the inputs for the step
         all_inputs = self.process.all_inputs(unique=True)
+        input_project_name=all_inputs[0].samples[0].project.name
+
+
 
         # check all input containers have the same type and also create list of unique containers
         container_types = set()
-        unique_containers=set()
+        unique_container_names=set()
 
 
         for artifact in all_inputs:
             container_types.add(artifact.container.type)
-            unique_containers.add(artifact.container)
+            unique_container_names.add(artifact.container.name)
+
+
 
         if len(container_types) > 1:
             raise ValueError('Only 1 container type is permitted. Multiple container types are present in the step')
@@ -75,42 +80,40 @@ class GenerateManifest96WellPlate(SendMailEPP):
 
         # create a dictionary of samples so the manifest can be written by column
         for artifact in all_inputs:
-            sample_dict[artifact.location[1].replace(":","")]=artifact.samples[0]
+            sample_dict[artifact.container.name+artifact.location[1].replace(":","")]=artifact.samples[0]
+
+        for container in sorted(unique_container_names):
+            for column,row in itertools.product(columns,rows):
+                if container+row+column not in sample_dict:
+                    continue
+                #if condition met then 'continue' means will skip back to beginning of loop
+
+                # populate the manifest with sample attributes
+                sample_udf=sample_dict[container+row+column].udf
+
+                #sample_udf = artifact.samples[0].udf
+                # populate the wells in the active work sheet in the excel in the columns defined by the step UDFs
+                if con_type == '[Plates]':
+                    ws[step_udfs[con_type + 'Sample Name'] + str(row_counter)] = sample_dict[container+row+column].artifact.name
+                    ws[step_udfs['[Plates]Container Name'] + str(row_counter)] = sample_dict[container+row+column].artifact.container.name
+                    ws[step_udfs['[Plates]Well'] + str(row_counter)] = sample_dict[container+row+column].artifact.location[1]
+                    ws[step_udfs['[Plates]Project ID'] + str(row_counter)] =sample_dict[container+row+column].project.name
+
+                # populate the manifest with sample UDFs which are configurable by adding or removing step UDFs in the
+                # format [CONTAINER TYPE - either Tubes or Plates][Sample UDF]Name of UDF
+                for configurable_udf in configurable_udfs:
+                    if con_type + '[Sample UDF]' + configurable_udf in step_udfs.keys():
+                        ws[step_udfs[con_type + '[Sample UDF]' + configurable_udf] + str(row_counter)] = \
+                            sample_udf[configurable_udf]
 
 
-        for column,row in itertools.product(columns,rows):
-            if row+column not in sample_dict:
-                continue
-            #if condition met then 'continue' means will skip back to beginning of loop
+                row_counter += 1
 
-            # populate the manifest with sample attributes
-            sample_udf=sample_dict[row+column].udf
-            #sample_udf = artifact.samples[0].udf
-            # populate the wells in the active work sheet in the excel in the columns defined by the step UDFs
-            if con_type == '[Plates]':
-                ws[step_udfs[con_type + 'Sample Name'] + str(row_counter)] = sample_dict[row+column].artifact.name
-                ws[step_udfs['[Plates]Container Name'] + str(row_counter)] = sample_dict[row+column].artifact.container.name
-                ws[step_udfs['[Plates]Well'] + str(row_counter)] = sample_dict[row+column].artifact.location[1]
-                ws[step_udfs['[Plates]Project ID'] + str(row_counter)] =sample_dict[row+column].project.name
+        if con_type == '[Tubes]':
+            ws[step_udfs['[Tubes]Project ID Well']] = input_project_name
 
-            if con_type == '[Tubes]':
-                ws[step_udfs['[Tubes]Project ID Well']] = sample_dict[row+column].project.name
-
-
-            if con_type == '[SGP]':
-                ws[step_udfs['[SGP]Project ID Well']] = sample_dict[row+column].project.name
-
-
-            # populate the manifest with sample UDFs which are configurable by adding or removing step UDFs in the
-            # format [CONTAINER TYPE - either Tubes or Plates][Sample UDF]Name of UDF
-            for configurable_udf in configurable_udfs:
-                if con_type + '[Sample UDF]' + configurable_udf in step_udfs.keys():
-                    ws[step_udfs[con_type + '[Sample UDF]' + configurable_udf] + str(row_counter)] = \
-                        sample_udf[configurable_udf]
-
-            row_counter += 1
-
-
+        if con_type == '[SGP]':
+            ws[step_udfs['[SGP]Project ID Well']] = input_project_name
 
         # create a new file with the original file name plus a suffix containing the project ID
         lims_filepath = self.manifest + '-'+'Edinburgh_Genomics_Sample_Submission_Manifest_' + all_inputs[0].samples[0].project.name + '.xlsx'
