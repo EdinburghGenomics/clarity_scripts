d529c8066afb6162e628e21edef3c7c8621ac08d
dforth <dan.forth@ed.ac.uk>
2019-02-18 15:30:19 +0000

    Script readded. Had been overwritten with test during manual renaming.

--- scripts/assign_workflow_post_receive_sample.py
+++ scripts/assign_workflow_post_receive_sample.py
@@ -1,44 +1,32 @@
-from unittest.mock import Mock, patch, PropertyMock, call
-
-from scripts.assign_workflow_post_receive_sample import AssignWorkflowPostReceiveSample
-from tests.test_common import TestEPP
+#!/usr/bin/env python
+from EPPs.common import StepEPP, get_workflow_stage
 
 
-def fake_all_inputs(unique=False, resolve=False):
-    """Return a list of mocked artifacts which contain samples with UDFs"""
-    return (
-        Mock(id='ai1', samples=[Mock(udf={'User Prepared Library': 'Yes'})]),
-        Mock(id='ai2', samples=[Mock(udf={'User Prepared Library': 'No'})])
-    )
+class AssignWorkflowPostReceiveSample(StepEPP):
+    """
+    Assigns a dispatched plate to either Sample QC and Plate Prep or User Prepared Library Plate Prep
+    """
+    def _run(self):
+        artifacts_to_route_userprepared = set()
+        artifacts_to_route_sampleqc = set()
+
+        for art in self.artifacts:
+            sample = art.samples[0]
+            if sample.udf.get("User Prepared Library") == "Yes":
+                artifacts_to_route_userprepared.add(art)
+            else:
+                artifacts_to_route_sampleqc.add(art)
+
+        if artifacts_to_route_userprepared:
+            # Only route artifacts if there are any artifacts to go to User Prepared Library
+            stage = get_workflow_stage(self.lims, "User Prepared Library Receipt and Batch EG 1.0 WF", "User Prepared Library Batch EG 1.0 ST")
+            self.lims.route_artifacts(list(artifacts_to_route_userprepared), stage_uri=stage.uri)
+
+        if artifacts_to_route_sampleqc:
+            # Only route artifacts if there are any artifacts to go to PreSeqLab
+            stage = get_workflow_stage(self.lims, "PreSeqLab EG 6.0", "Spectramax Picogreen EG 6.0")
+            self.lims.route_artifacts(list(artifacts_to_route_sampleqc), stage_uri=stage.uri)
 
 
-class TestAssignWorkflowReceiveSample(TestEPP):
-    def setUp(self):
-        self.patched_process = patch.object(
-            AssignWorkflowPostReceiveSample,
-            'process',
-            new_callable=PropertyMock(return_value=Mock(all_inputs=fake_all_inputs))
-        )
-        self.patched_get_workflow_stage = patch(
-            'scripts.assign_workflow_post_receive_sample.get_workflow_stage',
-            return_value=Mock(uri='a_uri')
-        )
-        self.epp = AssignWorkflowPostReceiveSample(self.default_argv)
-
-    def test_assign(self):
-        with self.patched_get_workflow_stage as pws, self.patched_lims, self.patched_process:
-            self.epp._run()
-
-            pws.assert_has_calls((
-                call(self.epp.lims, 'User Prepared Library Receipt and Batch EG 1.0 WF',
-                     'User Prepared Library Batch EG 1.0 ST'),
-                call(self.epp.lims, 'PreSeqLab EG 6.0', 'Spectramax Picogreen EG 6.0')
-            ))
-            # first routing (user prepared library)
-            route_args = self.epp.lims.route_artifacts.call_args_list[0]
-            assert sorted([a.id for a in route_args[0][0]]) == ['ai1']
-            assert self.epp.lims.route_artifacts.call_args[1] == {'stage_uri': 'a_uri'}
-
-            # second routing (not user prepared library)
-            route_args = self.epp.lims.route_artifacts.call_args_list[1]
-            assert sorted([a.id for a in route_args[0][0]]) == ['ai2']
+if __name__ == "__main__":
+    AssignWorkflowPostReceiveSample().run()
