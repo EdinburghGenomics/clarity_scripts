5307053692878b294fc61e9066127c7364e22ccb
dforth <dan.forth@ed.ac.uk>
2019-03-12 11:59:00 +0000

    next_step_assignment_kapa_qc.py updated so qPCR samples are assigned by rework

--- scripts/next_step_assignment_kapa_qc.py
+++ scripts/next_step_assignment_kapa_qc.py
@@ -20,15 +20,25 @@ class AssignNextStep(StepEPP):
         current_step = self.process.step.configuration  # configuration gives the ProtocolStep entity.
         protocol = Protocol(self.process.lims, uri='/'.join(self.process.step.configuration.uri.split('/')[:-2]))
         steps = protocol.steps  # a list of all the ProtocolSteps in protocol
-        step_object = steps[steps.index(current_step) + 1]
+        next_step_object = steps[steps.index(current_step) + 1]
 
         for next_action in next_actions:
+
             # if KAPA Next Step is KAPA Make Normalised Libraries then assign to next step in the protocol (assumes next step is KAPA Make Normalised Libraries)
 
             if next_action['artifact'].udf.get('KAPA Next Step') == 'KAPA Make Normalised Libraries':
                 # update the next action to "next step" with the step
                 next_action['action'] = 'nextstep'
-                next_action['step'] = step_object
+                next_action['step'] = next_step_object
+
+            elif next_action['artifact'].udf.get('KAPA Next Step') == 'Make and Read qPCR Quant':
+                #update the next action to re-work at Make and Read qPCR Quant
+                next_action['action'] = 'rework'
+                #find one instance of the qPCR step that the artifacts has been through
+                qpcr_process = self.lims.get_processes(type='Make and Read qPCR Quant EG 1.0 ST',
+                                                                     inputartifactlimsid=next_action['artifact'].id)[0]
+
+                next_action['rework-step'] =qpcr_process.step
 
             # all other artifacts should be route to remove from workflow as the submitted sample will reassigned elsewhere
             else:
