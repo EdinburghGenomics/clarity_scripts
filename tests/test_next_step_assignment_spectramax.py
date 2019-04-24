7228ed14cbde58c0a66bcacbfff8523d26b5c4b4
dforth <rolm393palp>
2019-04-24 14:57:55 +0100

    Test updated

--- tests/test_next_step_assignment_spectramax.py
+++ tests/test_next_step_assignment_spectramax.py
@@ -1,15 +1,10 @@
 from unittest.mock import patch, PropertyMock, Mock
 
-from EPPs.common import StepEPP
 from scripts.next_step_assignment_spectramax import AssignmentNextStepSpectramax
 from tests.test_common import TestEPP, NamedMock
 
 
-def fake_get_processes(type=None, inputartifactlimsid=None):
-    return [mock_process]
-
-
-def fake_outputs_per_input(id, ResultFile=None):
+def fake_outputs_per_input(id,ResultFile=None):
     fake_outputs = {
         'ai1': [Mock(udf={})],
         'ai2': [Mock(udf={})],
@@ -18,17 +13,17 @@ def fake_outputs_per_input(id, ResultFile=None):
     }
     return fake_outputs[id]
 
+
 class TestAssignmentNextStepSpectramax(TestEPP):
 
-
     def setUp(self):
-
         self.protostep = NamedMock(real_name='SeqPlatePrepST', uri='http://test.com/config/protocols/1/step/2')
         self.actions = Mock(
-            next_actions=[{'artifact': Mock(id='ai1')},
+            next_actions=[{'artifact': Mock(id='ai1', samples=[Mock(udf={})])},
                           {'artifact': Mock(id='ai2', samples=[Mock(udf={'Prep Workflow': 'KAPA DNA Library Prep'})])},
-                          {'artifact': Mock(id='ai3')},
-                          {'artifact': Mock(id='ai4', samples=[Mock(udf={'PreSeqLab Fragment Analyser Complete': True})])}
+                          {'artifact': Mock(id='ai3', samples=[Mock(udf={})])},
+                          {'artifact': Mock(id='ai4',
+                                            samples=[Mock(udf={'PreSeqLab Fragment Analyser Complete': True})])}
                           ]
         )
 
@@ -39,10 +34,9 @@ class TestAssignmentNextStepSpectramax(TestEPP):
                 step=Mock(actions=self.actions, configuration=self.protostep), outputs_per_input=fake_outputs_per_input)
             ))
 
-        protocol=Mock(steps=[self.protostep,Mock(),Mock(),Mock()])
-        self.patched_protocol = patch('scripts.next_step_assignment_spectramax.Protocol', return_value=protocol)
+        self.protocol = Mock(steps=[self.protostep, Mock(), Mock(), Mock()])
+        self.patched_protocol = patch('scripts.next_step_assignment_spectramax.Protocol', return_value=self.protocol)
         self.epp = AssignmentNextStepSpectramax(self.default_argv)
-
 
     def test_next_step_assignment_spectramax(self):
         with self.patched_protocol, self.patched_process:
