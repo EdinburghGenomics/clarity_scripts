4a8e12a68c1799d4634c3a2ead0764cc13e173d4
dforth <dan.forth@ed.ac.uk>
2019-05-14 13:36:17 +0100

    new test added for inputs including pools

--- tests/test_populate_batch_id_and_recipe.py
+++ tests/test_populate_batch_id_and_recipe.py
@@ -12,6 +12,7 @@ class TestPopulateBatchIDRecipe (TestEPP):
         self.fem=FakeEntitiesMaker()
 
     def test_no_pools(self):
+        #test that CST batch ID is assigned correctly and that correct recipe is assigned for non-pooling
         self.epp.lims = self.fem.lims
         self.epp.process = self.fem.create_a_fake_process()
         self.epp.lims.get_containers = Mock(return_value=[])
@@ -24,4 +25,17 @@ class TestPopulateBatchIDRecipe (TestEPP):
         actual_recipe = self.epp.process.udf['Sequencing Run Configuration']
 
         assert actual_batch_id == expected_batch_id
+        assert actual_recipe == expected_recipe
+
+    def test_pools(self):
+        #test that CST batch ID is assigned correctly and that correct recipe is assigned for pooling
+        self.epp.lims = self.fem.lims
+        self.epp.process = self.fem.create_a_fake_process(nb_input=2,nb_input_container=2,
+                                                          input_container_name=iter(['LP9999999-NTP','LP9999999-PDP']))
+        self.epp.lims.get_containers = Mock(return_value=[])
+        self.epp._run()
+
+        expected_recipe = 'HiSeqXRecipe-Pooled.xml'
+        actual_recipe = self.epp.process.udf['Sequencing Run Configuration']
+
         assert actual_recipe == expected_recipe
